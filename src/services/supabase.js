750a3cd7e5e558c533104c12ee6795aadebb548c
Maor <maorsa9@gmail.com>
2023-10-01 14:02:47 +0300

    Connect the supbase to the project

---
+++ src/services/supabase.js
@@ -0,0 +1,8 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://ryfhoebszdjwdsoibokl.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ5ZmhvZWJzemRqd2Rzb2lib2tsIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTYxNTA3NzQsImV4cCI6MjAxMTcyNjc3NH0.lvbVpKipYcnF-KBEo3ESNqtTPHJm1ijNppKRHWO48Ds";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
