12ff374c77dd8097cd59436f41866709280e3020
Maor <maorsa9@gmail.com>
2023-10-02 19:08:08 +0300

    Working with the creating editing and deleting Cabin with tosat message and uplodaing a file

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://ryfhoebszdjwdsoibokl.supabase.co";
+export const supabaseUrl = "https://ryfhoebszdjwdsoibokl.supabase.co";
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ5ZmhvZWJzemRqd2Rzb2lib2tsIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTYxNTA3NzQsImV4cCI6MjAxMTcyNjc3NH0.lvbVpKipYcnF-KBEo3ESNqtTPHJm1ijNppKRHWO48Ds";
 const supabase = createClient(supabaseUrl, supabaseKey);
